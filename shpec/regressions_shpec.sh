fe6021af265c1178a44768945b2ced8fe7a66100
Eric Villard <dev@eviweb.fr>
2016-04-27 23:59:52 +0200

    add regression tests for is_function
    Signed-off-by: Eric Villard <dev@eviweb.fr>

---
+++ shpec/regressions_shpec.sh
@@ -0,0 +1,20 @@
+describe "Regression tests"
+    describe "is_function"
+        it "returns 0 for function type even for non English locales"
+            stub_command "type" "[ \"\$1\" = \"-t\" ] && echo function || echo \"\$2 est une fonction\""
+            is_function "custom_assertion"
+            assert equal "0" "$?"
+            stub_command "type"
+        end
+
+        it "returns 1 for file type"
+            is_function "rmdir"
+            assert equal "1" "$?"
+        end
+
+        it "returns 1 for builtins type"
+            is_function "alias"
+            assert equal "1" "$?"
+        end
+    end
+end
